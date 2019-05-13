97b7b2251851888b7dbcce5ebd82f512e063494d
a-slide <aleg@ebi.ac.uk>
2019-05-13 23:07:44 +0100

    Remove tempfile for sort and work in wordir instead

--- NanoSnake/wrappers/minimap2_align/wrapper.py
+++ NanoSnake/wrappers/minimap2_align/wrapper.py
@@ -6,6 +6,7 @@ __license__ = "MIT"
 # Imports
 from snakemake.shell import shell
 import tempfile
+import os
 
 # Get optional args if unavailable
 opt = snakemake.params.get("opt", "")
@@ -19,7 +20,7 @@ align_threads = threads - (view_threads + sort_threads)
 shell("echo '#### MINIMAP2 + SAMTOOLS VIEW & SORT LOG ####' > {snakemake.log}")
 
 # Open temp directory for samtools sort temporary files
-with tempfile.TemporaryDirectory() as temp_dir:
+with tempfile.TemporaryDirectory(dir=os.path.dirname(snakemake.output[0])) as temp_dir:
     shell("minimap2 -t {align_threads} -a -L {opt} {snakemake.input.index} {snakemake.input.fastq} 2>> {snakemake.log}|\
         samtools view -@ {view_threads}  -bh 2>> {snakemake.log} |\
         samtools sort -@ {sort_threads} -T {temp_dir} -O bam > {snakemake.output[0]} 2>> {snakemake.log}")
--- NanoSnake/wrappers/samtools_filter/wrapper.py
+++ NanoSnake/wrappers/samtools_filter/wrapper.py
@@ -6,6 +6,7 @@ __license__ = "MIT"
 # Imports
 from snakemake.shell import shell
 import tempfile
+import os
 
 # Get optional args if unavailable
 opt = snakemake.params.get("opt", "")
@@ -19,7 +20,7 @@ sort_threads = threads-view_threads
 shell("echo '#### SAMTOOLS VIEW & SORT LOG ####' > {snakemake.log}")
 
 # Open temp directory for samtools sort temporary files
-with tempfile.TemporaryDirectory() as temp_dir:
+with tempfile.TemporaryDirectory(dir=os.path.dirname(snakemake.output[0])) as temp_dir:
     shell("samtools view -bh {opt} -@ {view_threads} {snakemake.input[0]} 2>> {snakemake.log}| \
         samtools sort -@ {sort_threads} -T {temp_dir} -O bam > {snakemake.output[0]}  2>> {snakemake.log}")
 
