5939ad94739330e8546f34f1cdbca23fd7ba78e2
a-slide <aleg@ebi.ac.uk>
2019-07-30 17:33:24 +0100

    Changes to accomodate fastq merger wrapper

--- NanoSnake/workflows/DNA_methylation/snakefile.py
+++ NanoSnake/workflows/DNA_methylation/snakefile.py
@@ -22,14 +22,14 @@ sample_list = list(sample_df.index)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Getters~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 def get_fastq (wildcards):
-    return glob (sample_df.loc[wildcards.sample, "fastq"])
+    return sample_df.loc[wildcards.sample, "fastq"]
 
-def get_fast5_dir (wildcards):
-    return glob (sample_df.loc[wildcards.sample, "fast5_dir"])
+def get_fast5 (wildcards):
+    return sample_df.loc[wildcards.sample, "fast5"]
 
 def get_seq_summary (wildcards):
     try:
-        return glob (sample_df.loc[wildcards.sample, "seq_summary"])
+        return sample_df.loc[wildcards.sample, "seq_summary"]
     except:
         return None
 
@@ -37,7 +37,7 @@ def get_seq_summary (wildcards):
 
 rule all:
     input:
-        expand(path.join("results","merge_fastq","{sample}.fastq"), sample=sample_list),
+        expand(path.join("results","merge_filter_fastq","{sample}.fastq"), sample=sample_list),
         expand(path.join("results","fastqc","{sample}_fastqc.html"), sample=sample_list),
         expand(path.join("results","fastqc","{sample}_fastqc.zip"), sample=sample_list),
         expand(path.join("results","minimap2_index","ref.mmi")),
@@ -48,27 +48,27 @@ rule all:
         expand(path.join("results","bamqc","{sample}_samtools_idxstats.txt"), sample=sample_list),
         expand(path.join("results","samtools_filter", "{sample}.bam"), sample=sample_list),
         expand(path.join("results","genomecov","{sample}.bedgraph"), sample=sample_list),
-        expand(path.join("results", "merge_fastq","{sample}.fastq.index"), sample=sample_list),
-        expand(path.join("results", "merge_fastq","{sample}.fastq.index.fai"), sample=sample_list),
-        expand(path.join("results", "merge_fastq","{sample}.fastq.index.gzi"), sample=sample_list),
-        expand(path.join("results", "merge_fastq","{sample}.fastq.index.readdb"), sample=sample_list),
+        expand(path.join("results", "merge_filter_fastq","{sample}.fastq.index"), sample=sample_list),
+        expand(path.join("results", "merge_filter_fastq","{sample}.fastq.index.fai"), sample=sample_list),
+        expand(path.join("results", "merge_filter_fastq","{sample}.fastq.index.gzi"), sample=sample_list),
+        expand(path.join("results", "merge_filter_fastq","{sample}.fastq.index.readdb"), sample=sample_list),
         expand(path.join("results", "nanopolish_call_methylation","{sample}_call_methylation.tsv"), sample=sample_list),
         expand(path.join("results", "nanopolish_call_methylation","{sample}_freq_meth_calculate.bed"), sample=sample_list),
         expand(path.join("results", "nanopolish_call_methylation","{sample}_freq_meth_calculate.tsv"), sample=sample_list),
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Rules~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-rule merge_fastq:
+rule merge_filter_fastq:
     input: get_fastq
-    output: path.join("results","merge_fastq","{sample}.fastq")
-    log: path.join("logs","merge_fastq","{sample}.log")
-    threads: config["merge_fastq"].get("threads", 1)
-    params: opt=config["merge_fastq"].get("opt", "")
-    resources: mem_mb=config["merge_fastq"].get("mem", 1000)
-    wrapper: "concat_files"
+    output: path.join("results","merge_filter_fastq","{sample}.fastq")
+    log: path.join("logs","merge_filter_fastq","{sample}.log")
+    threads: config["merge_filter_fastq"].get("threads", 1)
+    params: opt=config["merge_filter_fastq"].get("opt", "")
+    resources: mem_mb=config["merge_filter_fastq"].get("mem", 1000)
+    wrapper: "merge_filter_fastq"
 
 rule fastqc:
-    input: rules.merge_fastq.output
+    input: rules.merge_filter_fastq.output
     output:
         html=path.join("results","fastqc","{sample}_fastqc.html"),
         zip=path.join("results","fastqc","{sample}_fastqc.zip")
@@ -90,7 +90,7 @@ rule minimap2_index:
 rule minimap2_align:
     input:
         index=rules.minimap2_index.output,
-        fastq=rules.merge_fastq.output
+        fastq=rules.merge_filter_fastq.output
     output: path.join("results","minimap2_align","{sample}.bam")
     log: path.join("logs","minimap2_align","{sample}.log")
     threads: config["minimap2_align"].get("threads", 4)
@@ -131,14 +131,14 @@ rule genomecov:
 
 rule nanopolish_index:
     input:
-        fastq = rules.merge_fastq.output,
-        fast5_dir = get_fast5_dir,
+        fastq = rules.merge_filter_fastq.output,
+        fast5 = get_fast5,
         seq_summary = get_seq_summary,
     output:
-        index = path.join("results", "merge_fastq","{sample}.fastq.index"),
-        fai = path.join("results", "merge_fastq","{sample}.fastq.index.fai"),
-        gzi = path.join("results", "merge_fastq","{sample}.fastq.index.gzi"),
-        readdb = path.join("results", "merge_fastq","{sample}.fastq.index.readdb"),
+        index = path.join("results", "merge_filter_fastq","{sample}.fastq.index"),
+        fai = path.join("results", "merge_filter_fastq","{sample}.fastq.index.fai"),
+        gzi = path.join("results", "merge_filter_fastq","{sample}.fastq.index.gzi"),
+        readdb = path.join("results", "merge_filter_fastq","{sample}.fastq.index.readdb"),
     log: path.join("logs","nanopolish_index","{sample}.log")
     threads: config["nanopolish_index"].get("threads", 2)
     params: opt=config["nanopolish_index"].get("opt", "")
@@ -147,7 +147,7 @@ rule nanopolish_index:
 
 rule nanopolish_call_methylation:
     input:
-        fastq = rules.merge_fastq.output,
+        fastq = rules.merge_filter_fastq.output,
         bam = rules.samtools_filter.output,
         ref = config["reference"],
     output:
@@ -156,6 +156,8 @@ rule nanopolish_call_methylation:
         tsv = path.join("results", "nanopolish_call_methylation","{sample}_freq_meth_calculate.tsv"),
     log: path.join("logs","nanopolish_call_methylation","{sample}.log")
     threads: config["nanopolish_call_methylation"].get("threads", 2)
-    params: opt=config["nanopolish_call_methylation"].get("opt", "")
+    params:
+        opt_nanopolish=config["nanopolish_call_methylation"].get("opt_nanopolish", ""),
+        opt_nanopolishcomp=config["nanopolish_call_methylation"].get("opt_nanopolishcomp", ""),
     resources: mem_mb=config["nanopolish_call_methylation"].get("mem", 1000)
     wrapper: "nanopolish_call_methylation"
