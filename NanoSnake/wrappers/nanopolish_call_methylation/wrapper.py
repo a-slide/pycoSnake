9240f2644afa9b86a16a42dcf64e9b3ef950e73c
a-slide <aleg@ebi.ac.uk>
2019-08-12 18:47:46 +0100

    Update with new version of NanopolishComp with lower mem footprint

--- NanoSnake/wrappers/nanopolish_call_methylation/wrapper.py
+++ NanoSnake/wrappers/nanopolish_call_methylation/wrapper.py
@@ -11,10 +11,6 @@ from snakemake.shell import shell
 opt_nanopolish = snakemake.params.get("opt_nanopolish", "")
 opt_nanopolishcomp = snakemake.params.get("opt_nanopolishcomp", "")
 
-# Get outdir and outprefix for Freq_meth_calculate
-outdir = os.path.dirname(snakemake.output.bed)
-outprefix = os.path.basename(snakemake.output.bed).replace("_freq_meth_calculate.bed", "")
-
 # Run shell commands
 shell("echo '#### NANOPOLISH INDEX LOG ####' > {snakemake.log}")
 shell("nanopolish index -d {snakemake.input.fast5} {snakemake.input.fastq} -s {snakemake.input.seq_summary} -v 2>> {snakemake.log}")
@@ -23,4 +19,4 @@ shell("echo '#### NANOPOLISH CALL-METHYLATION LOG ####' > {snakemake.log}")
 shell("nanopolish call-methylation -t {snakemake.threads} {opt_nanopolish} -r {snakemake.input.fastq} -b {snakemake.input.bam} -g {snakemake.input.ref} > {snakemake.output.call} 2>> {snakemake.log}")
 
 shell("echo '#### NANOPOLISHCOMP FREQ_METH_CALCULATE LOG ####' >> {snakemake.log}")
-shell("NanopolishComp Freq_meth_calculate {opt_nanopolishcomp} -i {snakemake.output.call} -o {outdir} -p {outprefix} 2>> {snakemake.log}")
+shell("NanopolishComp Freq_meth_calculate --verbose {opt_nanopolishcomp} -i {snakemake.output.call} -b {snakemake.output.bed} -t {snakemake.output.tsv} 2>> {snakemake.log}")
