a70cfea2ccfe018e32e76c05d78d8d9367ad0772
a-slide <aleg@ebi.ac.uk>
2019-10-03 13:19:43 +0100

    Add report_title option to pycoQC

--- NanoSnake/wrappers/pycoqc/wrapper.py
+++ NanoSnake/wrappers/pycoqc/wrapper.py
@@ -10,5 +10,7 @@ from snakemake.shell import shell
 # Get optional args if unavailable
 opt = snakemake.params.get("opt", "")
 
+sample_id = os.path.split(snakemake.input.bam[0])[1].rpartition(".")[0]
+
 # Run shell command
-shell ("pycoQC {opt} --verbose -f {snakemake.input.seq_summary} -a {snakemake.input.bam} -o {snakemake.output.html} -j {snakemake.output.json} &> {snakemake.log}")
+shell ("pycoQC {opt} --verbose -f {snakemake.input.seq_summary} -a {snakemake.input.bam} --report_title {sample_id} -o {snakemake.output.html} -j {snakemake.output.json} &> {snakemake.log}")
