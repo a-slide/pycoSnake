3c5982603ad4a6733e65ee1d8778c06b5eba1eaf
a-slide <aleg@ebi.ac.uk>
2019-10-10 11:29:06 +0100

    Bump up version number

--- NanoSnake/__init__.py
+++ NanoSnake/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.0.0.29'
+__version__ = '0.0.0.30'
 __description__="NanoSnake is a neatly wrapped collection of snakemake workflows for analysing nanopore sequencing data"
