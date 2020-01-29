d029a855f017b607d9581b77a8e8e445751fb2ca
a-slide <aleg@ebi.ac.uk>
2020-01-29 11:41:49 +0000

    Update pipeline wrappers and add Salmon rules

--- NanoSnake/workflows/RNA_illumina/snakefile.py
+++ NanoSnake/workflows/RNA_illumina/snakefile.py
@@ -16,9 +16,9 @@ HTTP = HTTPRemoteProvider()
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~check config file version~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Minimum snakemake version
-config_version=3
+config_version=5
 if not "config_version" in config or config["config_version"]!= config_version:
-    raise NanoSnakeError ("Wrong configuration file version. Please regenerate config with -t config")
+    raise NanoSnakeError ("Wrong configuration file version. Please regenerate config with `--generate_template config -o`")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~Define samples sheet reference and getters~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 sample_df=pd.read_csv (config["sample_sheet"], comment="#", skip_blank_lines=True, sep="\t", index_col=0)
@@ -35,47 +35,51 @@ input_d=defaultdict(OrderedDict)
 output_d=defaultdict(OrderedDict)
 log_d=OrderedDict()
 
-rule_name="preprocess_genone"
-genome = config["genome"]
-if genome.startswith("ftp"):
-    input_d[rule_name]["ref"]=FTP.remote(genome)
-elif genome.startswith("http"):
-    input_d[rule_name]["ref"]=HTTP.remote(genome)
-else:
-    input_d[rule_name]["ref"]=genome
-output_d[rule_name]["ref"]=join("results","main","genone","ref.fa")
-output_d[rule_name]["index"]=join("results","main","genone","ref.fa.fai")
-log_d[rule_name]=join("logs",rule_name,"preprocess_genone.log")
+rule_name="get_genome"
+ref = config["genome"]
+if ref.startswith("ftp"): input_d[rule_name]["ref"]=FTP.remote(ref)
+elif ref.startswith("http"): input_d[rule_name]["ref"]=HTTP.remote(ref)
+else: input_d[rule_name]["ref"]=ref
+output_d[rule_name]["ref"]=join("results","input","genone","genome.fa")
+output_d[rule_name]["index"]=join("results","input","genone","genome.fa.fai")
+log_d[rule_name]=join("logs",rule_name,"get_genome.log")
 
-rule_name="preprocess_annotation"
-annotation = config["annotation"]
-if annotation.startswith("ftp"):
-    input_d[rule_name]["annotation"]=FTP.remote(annotation)
-elif annotation.startswith("http"):
-    input_d[rule_name]["annotation"]=HTTP.remote(annotation)
-else:
-    input_d[rule_name]["annotation"]=annotation
-output_d[rule_name]["gff3"]=join("results","main","annotation","ref.gff3")
-output_d[rule_name]["gtf"]=join("results","main","annotation","ref.gtf")
-log_d[rule_name]=join("logs",rule_name,"preprocess_annotation.log")
+rule_name="get_transcriptome"
+ref = config["transcriptome"]
+if ref.startswith("ftp"): input_d[rule_name]["ref"]=FTP.remote(ref)
+elif ref.startswith("http"): input_d[rule_name]["ref"]=HTTP.remote(ref)
+else: input_d[rule_name]["ref"]=ref
+output_d[rule_name]["ref"]=join("results","input","transcriptome","transcriptome.fa")
+output_d[rule_name]["tsv"]=join("results","input","transcriptome","transcriptome.tsv")
+output_d[rule_name]["index"]=join("results","input","transcriptome","transcriptome.fa.fai")
+log_d[rule_name]=join("logs",rule_name,"get_transcriptome.log")
+
+rule_name="get_annotation"
+gff3 = config["annotation"]
+if gff3.startswith("ftp"): input_d[rule_name]["gff3"]=FTP.remote(gff3)
+elif gff3.startswith("http"): input_d[rule_name]["gff3"]=HTTP.remote(gff3)
+else: input_d[rule_name]["gff3"]=gff3
+output_d[rule_name]["gff3"]=join("results","input","annotation","annotation.gff3")
+output_d[rule_name]["gtf"]=join("results","input","annotation","annotation.gtf")
+log_d[rule_name]=join("logs",rule_name,"get_annotation.log")
 
 rule_name="fastp"
 input_d[rule_name]["fastq1"]=get_fastq1
 input_d[rule_name]["fastq2"]=get_fastq2
-output_d[rule_name]["fastq1"]=join("results","main","fastp","{sample}_1.fastq.gz")
-output_d[rule_name]["fastq2"]=join("results","main","fastp","{sample}_2.fastq.gz")
+output_d[rule_name]["fastq1"]=join("results","input","fastp","{sample}_1.fastq.gz")
+output_d[rule_name]["fastq2"]=join("results","input","fastp","{sample}_2.fastq.gz")
 output_d[rule_name]["html"]=join("results","QC","fastp","{sample}_fastp.html")
 output_d[rule_name]["json"]=join("results","QC","fastp","{sample}_fastp.json")
 log_d[rule_name]=join("logs",rule_name,"{sample}.log")
 
 rule_name="star_index"
-input_d[rule_name]["ref"]=output_d["preprocess_genone"]["ref"]
-input_d[rule_name]["annotation"]=output_d["preprocess_annotation"]["gtf"]
-output_d[rule_name]["index"]=join("results","main","star_index","SAindex")
+input_d[rule_name]["ref"]=output_d["get_genome"]["ref"]
+input_d[rule_name]["annotation"]=output_d["get_annotation"]["gtf"]
+output_d[rule_name]["index_dir"]=directory(join("results","main","star_index"))
 log_d[rule_name]=join("logs",rule_name,"ref.log")
 
 rule_name="star_align"
-input_d[rule_name]["index"]=output_d["star_index"]["index"]
+input_d[rule_name]["index_dir"]=output_d["star_index"]["index_dir"]
 input_d[rule_name]["fastq1"]=output_d["fastp"]["fastq1"]
 input_d[rule_name]["fastq2"]=output_d["fastp"]["fastq2"]
 output_d[rule_name]["sj"]=join("results","main","star_alignments","{sample}_SJ.tsv")
@@ -100,8 +104,8 @@ log_d[rule_name]=join("logs",rule_name,"star_count_merge.log")
 
 rule_name="cufflinks"
 input_d[rule_name]["bam"]=output_d["star_align"]["bam"]
-input_d[rule_name]["ref"]=output_d["preprocess_genone"]["ref"]
-input_d[rule_name]["annotation"]=output_d["preprocess_annotation"]["gtf"]
+input_d[rule_name]["ref"]=output_d["get_genome"]["ref"]
+input_d[rule_name]["annotation"]=output_d["get_annotation"]["gtf"]
 output_d[rule_name]["genes_fpkm"]=join("results","counts","cufflinks","{sample}_genes_fpkm.tsv")
 output_d[rule_name]["isoforms_fpkm"]=join("results","counts","cufflinks","{sample}_isoforms_fpkm.tsv")
 log_d[rule_name]=join("logs",rule_name,"{sample}.log")
@@ -115,7 +119,7 @@ log_d[rule_name]=join("logs",rule_name,"cufflinks_fpkm_merge.log")
 
 rule_name="subread_featurecounts"
 input_d[rule_name]["bam"]=output_d["star_align"]["bam"]
-input_d[rule_name]["gtf"]=output_d["preprocess_annotation"]["gtf"]
+input_d[rule_name]["gtf"]=output_d["get_annotation"]["gtf"]
 output_d[rule_name]["counts"]=join("results","counts","featurecounts","{sample}_counts.tsv")
 log_d[rule_name]=join("logs",rule_name,"{sample}.log")
 
@@ -139,14 +143,35 @@ log_d[rule_name]=join("logs",rule_name,"{sample}.log")
 
 rule_name="igvtools_count"
 input_d[rule_name]["bam"]=output_d["pbt_alignment_filter"]["bam"]
-input_d[rule_name]["ref"]=output_d["preprocess_genone"]["ref"]
+input_d[rule_name]["ref"]=output_d["get_genome"]["ref"]
 output_d[rule_name]["tdf"]=join("results","coverage","igv_tdf","{sample}.tdf")
 log_d[rule_name]=join("logs",rule_name,"{sample}.log")
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~Define all output depending on config file~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+rule_name="salmon_index"
+input_d[rule_name]["ref"]=output_d["get_transcriptome"]["ref"]
+output_d[rule_name]["index_dir"]=directory(join("results","main","salmon_index"))
+log_d[rule_name]=join("logs",rule_name,"ref.log")
 
+rule_name="salmon_quant"
+input_d[rule_name]["index_dir"]=output_d["salmon_index"]["index_dir"]
+input_d[rule_name]["fastq1"]=output_d["fastp"]["fastq1"]
+input_d[rule_name]["fastq2"]=output_d["fastp"]["fastq2"]
+output_d[rule_name]["quant_dir"]=directory(join("results","main","salmon_quant","{sample}"))
+output_d[rule_name]["counts"]=join("results","counts","salmon_quant","{sample}_counts.tsv")
+log_d[rule_name]=join("logs",rule_name,"{sample}.log")
+
+rule_name="salmon_count_merge"
+input_d[rule_name]["counts"]=[join("results","counts","salmon_quant",f"{sample}_counts.tsv") for sample in sample_list]
+output_d[rule_name]["counts"]=join("results","counts","salmon_count_merge","counts.tsv")
+output_d[rule_name]["tpm"]=join("results","counts","salmon_count_merge","tpm.tsv")
+log_d[rule_name]=join("logs",rule_name,"salmon_count_merge.log")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~Define all output depending on config file~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # main rules
-run_rules=["preprocess_genone", "preprocess_annotation", "fastp", "star_index", "star_align", "pbt_alignment_filter", "star_count_merge"]
+run_rules=[]
+run_rules.extend(["get_genome", "get_transcriptome", "get_annotation", "fastp"])
+run_rules.extend(["star_index", "star_align", "pbt_alignment_filter", "star_count_merge"])
+run_rules.extend(["salmon_index", "salmon_quant", "salmon_count_merge"])
 
 # Optional cufflinks analysis
 opt_rules = ["cufflinks", "cufflinks_fpkm_merge"]
@@ -162,7 +187,7 @@ if all_in(config, opt_rules):
 opt_rules = ["samtools_qc", "bedtools_genomecov", "igvtools_count"]
 for r in opt_rules:
     if r in config:
-        run_rules.extend(opt_rules)
+        run_rules.append(r)
 
 all_output=[]
 for rule_name, rule_d in output_d.items():
@@ -175,25 +200,35 @@ for rule_name, rule_d in output_d.items():
 rule all:
     input: [expand(o, sample=sample_list) for o in all_output]
 
-rule_name="preprocess_genone"
-rule preprocess_genone:
+rule_name="get_genome"
+rule get_genome:
     input: **input_d[rule_name]
     output: **output_d[rule_name]
     log: log_d[rule_name]
     threads: get_threads(config, rule_name)
     params: opt=get_opt(config, rule_name)
     resources: mem_mb=get_mem(config, rule_name)
-    wrapper: "preprocess_genone"
+    wrapper: "get_genome"
 
-rule_name="preprocess_annotation"
-rule preprocess_annotation:
+rule_name="get_transcriptome"
+rule get_transcriptome:
     input: **input_d[rule_name]
     output: **output_d[rule_name]
     log: log_d[rule_name]
     threads: get_threads(config, rule_name)
     params: opt=get_opt(config, rule_name)
     resources: mem_mb=get_mem(config, rule_name)
-    wrapper: "preprocess_annotation"
+    wrapper: "get_transcriptome"
+
+rule_name="get_annotation"
+rule get_annotation:
+    input: **input_d[rule_name]
+    output: **output_d[rule_name]
+    log: log_d[rule_name]
+    threads: get_threads(config, rule_name)
+    params: opt=get_opt(config, rule_name)
+    resources: mem_mb=get_mem(config, rule_name)
+    wrapper: "get_annotation"
 
 rule_name="fastp"
 rule fastp:
@@ -314,3 +349,33 @@ rule igvtools_count:
     params: opt=get_opt(config, rule_name)
     resources: mem_mb=get_mem(config, rule_name)
     wrapper: "igvtools_count"
+
+rule_name="salmon_index"
+rule salmon_index:
+    input: **input_d[rule_name]
+    output: **output_d[rule_name]
+    log: log_d[rule_name]
+    threads: get_threads(config, rule_name)
+    params: opt=get_opt(config, rule_name)
+    resources: mem_mb=get_mem(config, rule_name)
+    wrapper: "salmon_index"
+
+rule_name="salmon_quant"
+rule salmon_quant:
+    input: **input_d[rule_name]
+    output: **output_d[rule_name]
+    log: log_d[rule_name]
+    threads: get_threads(config, rule_name)
+    params: opt=get_opt(config, rule_name)
+    resources: mem_mb=get_mem(config, rule_name)
+    wrapper: "salmon_quant"
+
+rule_name="salmon_count_merge"
+rule salmon_count_merge:
+    input: **input_d[rule_name]
+    output: **output_d[rule_name]
+    log: log_d[rule_name]
+    threads: get_threads(config, rule_name)
+    params: opt=get_opt(config, rule_name)
+    resources: mem_mb=get_mem(config, rule_name)
+    wrapper: "salmon_count_merge"
