92a821e33c9c9d8407d0b0bd9c07bfd06a7a50f3
a-slide <aleg@ebi.ac.uk>
2020-01-31 15:06:54 +0000

    Update pbt wrappers

--- NanoSnake/wrappers/pbt_alignment_filter/wrapper.py
+++ NanoSnake/wrappers/pbt_alignment_filter/wrapper.py
@@ -6,8 +6,6 @@ __version__ = "0.0.1"
 
 # Imports
 from snakemake.shell import shell
-import tempfile
-import os
 
 # Shortcuts
 opt = snakemake.params.get("opt", "")
@@ -15,4 +13,4 @@ bam_input = snakemake.input.bam
 bam_output = snakemake.output.bam
 
 # Run shell command
-shell("pyBioTools Alignment Filter -i {bam_input} -o {bam_output} {opt} --verbose &> {snakemake.log}")
+shell("pyBioTools Alignment Filter {opt} -i {bam_input} -o {bam_output} --verbose &> {snakemake.log}")
