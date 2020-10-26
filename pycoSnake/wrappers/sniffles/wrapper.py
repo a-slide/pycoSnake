6b8705867257466c916168c08201bf906a98d9e0
a-slide <aleg@ebi.ac.uk>
2020-10-26 10:31:58 +0000

    Remove sort in sniffles wrapper

--- pycoSnake/wrappers/sniffles/wrapper.py
+++ pycoSnake/wrappers/sniffles/wrapper.py
@@ -5,7 +5,7 @@ import os
 
 # Wrapper info
 wrapper_name = "sniffles"
-wrapper_version = "0.0.2"
+wrapper_version = "0.0.3"
 author = "Adrien Leger"
 license = "MIT"
 shell("echo 'Wrapper {wrapper_name} v{wrapper_version} / {author} / Licence {license}' > {snakemake.log}")
@@ -20,16 +20,11 @@ output_vcf = snakemake.output.vcf
 with tempfile.TemporaryDirectory() as temp_dir:
 
     # Temorary files
-    temp_vcf = os.path.join(temp_dir, "temp.vcf")
     temp_sniffles = os.path.join(temp_dir, "temp.snf")
 
     # Call variant with or without input vcf
     shell("echo '#### SNIFFLES LOG ####' >> {snakemake.log}")
     if input_vcf:
-        shell("sniffles {opt} -t {snakemake.threads} -m {input_bam} --Ivcf {input_vcf} -v {temp_vcf} --tmp_file {temp_sniffles} &>> {snakemake.log}")
+        shell("sniffles {opt} -t {snakemake.threads} -m {input_bam} --Ivcf {input_vcf} -v {output_vcf} --tmp_file {temp_sniffles} &>> {snakemake.log}")
     else:
-        shell("sniffles {opt} -t {snakemake.threads} -m {input_bam} -v {temp_vcf} --tmp_file {temp_sniffles} &>> {snakemake.log}")
-
-    # sort VCF
-    shell("echo '#### BCFTOOLS SORT LOG ####' >> {snakemake.log}")
-    shell("bcftools sort {temp_vcf} -o {output_vcf} -O v -T {temp_dir} &>> {snakemake.log}")
+        shell("sniffles {opt} -t {snakemake.threads} -m {input_bam} -v {output_vcf} --tmp_file {temp_sniffles} &>> {snakemake.log}")
